8a05ee1c8778e7f319690bea1f4014415aca9219
刘宇 <506610466@qq.com>
2024-03-09 23:28:03 +0800

    修改 mian的实现

--- gpt_server/serving/main.py
+++ gpt_server/serving/main.py
@@ -39,52 +39,53 @@ port = config["serve_args"]["port"]
 start_server(host, port)
 # ----------------------------启动 Controller 和 Openai API 服务----------------------------------------------------
 process = []
-for model_name, model_config in config["models"].items():
-    # 启用的模型
-    if model_config["enable"]:
-        # pprint(model_config)
-        print()
-        # 模型地址
-        model_name_or_path = model_config["model_name_or_path"]
-        # 模型类型
-        model_type = model_config["model_type"]
+for model_config_ in config["models"]:
+    for model_name, model_config in model_config_.items():
+        # 启用的模型
+        if model_config["enable"]:
+            # pprint(model_config)
+            print()
+            # 模型地址
+            model_name_or_path = model_config["model_name_or_path"]
+            # 模型类型
+            model_type = model_config["model_type"]
 
-        # model type 校验
-        # py_path = f"{root_dir}/gpt_server/model_worker/{model_type}.py"
-        py_path = f"-m gpt_server.model_worker.{model_type}"
+            # model type 校验
+            # py_path = f"{root_dir}/gpt_server/model_worker/{model_type}.py"
+            py_path = f"-m gpt_server.model_worker.{model_type}"
 
-        model_names = model_name
-        if model_config["alias"]:
-            model_names = model_name + "," + model_config["alias"]
+            model_names = model_name
+            if model_config["alias"]:
+                model_names = model_name + "," + model_config["alias"]
 
-        # 获取 worker 数目 并获取每个 worker 的资源
-        workers = model_config["workers"]
-        # if model_config["work_mode"] == "deepspeed":
-        # 设置使用 deepspeed
+            # 获取 worker 数目 并获取每个 worker 的资源
+            workers = model_config["workers"]
+            # if model_config["work_mode"] == "deepspeed":
+            # 设置使用 deepspeed
 
-        # process = []
-        for worker in workers:
-            gpus = worker["gpus"]
-            # 将gpus int ---> str
-            gpus = [str(i) for i in gpus]
-            gpus_str = ",".join(gpus)
-            num_gpus = len(gpus)
-            run_mode = "python "
-            os.environ["CUDA_VISIBLE_DEVICES"] = gpus_str
+            # process = []
+            for worker in workers:
+                gpus = worker["gpus"]
+                # 将gpus int ---> str
+                gpus = [str(i) for i in gpus]
+                gpus_str = ",".join(gpus)
+                num_gpus = len(gpus)
+                run_mode = "python "
+                os.environ["CUDA_VISIBLE_DEVICES"] = gpus_str
 
-            if model_config["work_mode"] == "vllm":
-                os.environ["USE_VLLM"] = "1"
+                if model_config["work_mode"] == "vllm":
+                    os.environ["USE_VLLM"] = "1"
 
-            cmd = (
-                run_mode
-                + py_path
-                + f" --gpus {gpus_str}"
-                + f" --model_name_or_path {model_name_or_path}"
-                + f" --model_names {model_names}"
-            )
+                cmd = (
+                    run_mode
+                    + py_path
+                    + f" --gpus {gpus_str}"
+                    + f" --model_name_or_path {model_name_or_path}"
+                    + f" --model_names {model_names}"
+                )
 
-            p = Process(target=run_cmd, args=(cmd,))
-            p.start()
-            process.append(p)
+                p = Process(target=run_cmd, args=(cmd,))
+                p.start()
+                process.append(p)
 for p in process:
     p.join()
