14b3b22ece0ffbb47efd82e2a671184f27ed401e
刘宇 <506610466@qq.com>
2024-10-21 16:56:07 +0800

    add project.scripts

--- gpt_server/cli.py
+++ gpt_server/cli.py
@@ -1,7 +1,26 @@
 import argparse
+import subprocess
+import os
+
+root_dir = os.path.dirname(__file__)
+root_dir = os.path.abspath(root_dir)
+chat_ui_path = os.path.join(root_dir, "serving", "chat_ui.py")
+server_ui_path = os.path.join(root_dir, "serving", "server_ui.py")
 
 
 def run():
-    parser = argparse.ArgumentParser()
-
+    parser = argparse.ArgumentParser(description="GPT Server CLI")
+    parser.add_argument("--chat_ui", action="store_true", help="启动问答UI界面")
+    parser.add_argument("--server_ui", action="store_true", help="启动服务UI界面")
     args = parser.parse_args()
+    print(args)
+    if args.chat_ui:
+        cmd = f"streamlit run {chat_ui_path}"
+        subprocess.run(cmd, shell=True)
+    if args.server_ui:
+        cmd = f"streamlit run {server_ui_path}"
+        subprocess.run(cmd, shell=True)
+
+
+if __name__ == "__main__":
+    run()
