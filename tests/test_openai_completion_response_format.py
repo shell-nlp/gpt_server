d7875d9c26cc2b0f910e24b4a88098d77f56e968
刘宇 <506610466@qq.com>
2024-11-27 15:20:48 +0800

    add test_response_format

---
+++ tests/test_openai_completion_response_format.py
@@ -0,0 +1,41 @@
+from openai import OpenAI
+from pydantic import BaseModel, Field
+
+# 新版本 opnai
+client = OpenAI(api_key="EMPTY", base_url="http://localhost:8082/v1")
+
+# 方式一
+output = client.chat.completions.create(
+    model="qwen-32b",
+    messages=[{"role": "user", "content": "南京到北京多远"}],
+    response_format={"type": "text"},
+)
+print(output.choices[0].message.content)
+print("-" * 100)
+# 方式二
+output = client.chat.completions.create(
+    model="qwen-32b",
+    messages=[
+        {"role": "system", "content": "用json进行回答"},
+        {"role": "user", "content": "南京到北京多远"},
+    ],
+    response_format={"type": "json_object"},
+)
+print(output.choices[0].message.content)
+print("-" * 100)
+
+
+# 方式三
+class Distance(BaseModel):
+    距离: int = Field()
+    单位: str
+
+
+output = client.beta.chat.completions.parse(
+    model="qwen-32b",
+    messages=[{"role": "user", "content": "南京到北京多远"}],
+    response_format=Distance,
+)
+
+print(output.choices[0].message.parsed.dict())
+print()
