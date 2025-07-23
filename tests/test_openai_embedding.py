db005871cef8f97a92b9069a1b76f9a14c0142e9
刘宇 <506610466@qq.com>
2025-07-23 13:56:32 +0800

    update test embebdding

--- tests/test_openai_embedding.py
+++ tests/test_openai_embedding.py
@@ -5,11 +5,15 @@ import numpy as np
 # 新版本 opnai
 client = OpenAI(api_key="EMPTY", base_url="http://localhost:8082/v1")
 # model: acge_text_embedding yinka zpoint
-response = client.embeddings.create(model="bge-m3", input=["我喜欢你", "我也喜欢你"])
+response = client.embeddings.create(
+    model="Conan-embedding-v1", input=["我喜欢你", "我也喜欢你"]
+)
 print(response.data)
 embeddings = [np.array(item.embedding) for item in response.data]  # 转为NumPy数组
+
 v_a = embeddings[0].reshape(1, -1)  # 向量a
 v_b = embeddings[1].reshape(-1, 1)  # 向量b
+print(v_a.shape)
 # 计算余弦相似度
 similarity = np.dot(v_a, v_b)[0][0]
 print(f"余弦相似度: {similarity:.4f}")
